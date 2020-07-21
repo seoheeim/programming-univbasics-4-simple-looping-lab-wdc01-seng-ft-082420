01ec17fd586c021162d6c4446c6625ceaf561054
Seohee  Im <seoheeim@vt.edu>
2020-07-21 19:12:35 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,8 +1,11 @@
 # Write your methods here
-def loop_message_five_times
-  count = 10 
+def loop_message_five_times(message)
+  message = "Hello World!"
+  count = 0
   while count < 5
-  
+  puts message
+  count += 1 
+end
 end
 
 def loop_message_n_times
